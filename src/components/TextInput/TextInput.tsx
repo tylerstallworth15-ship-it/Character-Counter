7e9b22f1b1e6a8ed3afb04415ae5f4f0b95528f1
Tyler Glen <tylerstallworth15@gmail.com>
2025-12-18 18:03:57 -0500

    textinput

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TextInputProps } from '../../types';
+
+export const TextInput: React.FC<TextInputProps> = ({
+    onTextChange,
+    placeholder= 'Start Typing...',
+    initialValue = ''
+}) => {
+  return (
+    <div className='w-full'>
+      <textarea
+        className='w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
+        placeholder={placeholder}
+        defaultValue={initialValue}
+        onChange={(e) => onTextChange(e.target.value)}
+        rows={6}
+      />
+    </div>
+  );
+};
