9b49290cfb442648b960f7661ca3ec82697092e5
Tyler Glen <tylerstallworth15@gmail.com>
2025-12-18 18:41:39 -0500

    CC skeleton

--- src/components/CharacterCounter/CharacterCounter.tsx
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -3,3 +3,27 @@ import { CharacterCounterProps, TextStats } from '../../types';
 import { TextInput } from '../TextInput/TextInput';
 import { StatsDisplay } from '../StatsDisplay/StatsDisplay';
 
+export const CharacterCounter: React.FC<CharacterCounterProps> = ({
+    minWords
+    maxWords
+}) => {
+    const [text, setText] = useState('')
+
+    Const
+    Const
+    Const
+    return { }
+};
+
+const
+
+return (
+    <div className="p-6 max-w-xl mx-auto">
+        <TextInput
+        <StatsDisplay 
+        <div className=
+          min:
+        </div>
+       </div>
+     );
+    };
