73a104ae5ee27962998b4f50f72662985fc9c60c
Tyler Glen <tylerstallworth15@gmail.com>
2025-12-18 18:29:09 -0500

    CC import

---
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { CharacterCounterProps, TextStats } from '../../types';
+import { TextInput } from '../TextInput/TextInput';
+import { StatsDisplay } from '../StatsDisplay/StatsDisplay';
+
