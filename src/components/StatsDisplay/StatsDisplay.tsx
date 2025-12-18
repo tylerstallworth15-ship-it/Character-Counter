0f5ec672a9574c57865ca6dfbae36f6b0bfb8d4b
Tyler Glen <tylerstallworth15@gmail.com>
2025-12-18 18:21:34 -0500

    SD com

--- src/components/StatsDisplay/StatsDisplay.tsx
+++ src/components/StatsDisplay/StatsDisplay.tsx
@@ -1,4 +1,20 @@
 import React from 'react';
 import { StatsDisplayProps } from '../../types';
 
-export const StatsDisplay: React.FC<StatsDisplayProps>
+export const StatsDisplay: React.FC<StatsDisplayProps> = ({ stats, showReadingTime = true }) => {
+    return (
+        <div className="mt-4 space-y-2">
+          <div>
+            <strong>Characters:</strong> {stats.characterCount}
+        </div>
+        <div>
+            <strong>Words: </strong> {stats.wordCount}
+        </div>
+        {showReadingTime && (
+          <div>
+            <strong>Reading Time:</strong> {stats.readingTime.toFixed(2)} minutes
+          </div>
+        )}
+        </div>
+    );
+};
